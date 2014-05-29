f329d0fbaa66467978d2ab3d2198f4d3fad06e3c
Michael Norris <github@michaelnorris.co.uk>
2014-05-29 11:29:45 +0100

    Remove container and add main.css

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -7,17 +7,11 @@
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     {{ HTML::style('//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css') }}
+    {{ HTML::style('css/main.css') }}
     <!-- Place favicon.ico and apple-touch-icon(s) in the root directory -->
 </head>
 <body>
-<!--[if lt IE 8]>
-<p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
-<![endif]-->
-<div class="container col-xs-10 col-xs-offset-1">
-    @yield('content')
-</div>
-
-
+@yield('content')
 {{ HTML::script('//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js') }}
 {{ HTML::script('//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js') }}
 </body>
