9d79e7d25273d426a382bf12e82f41d556cd3709
Michael Norris <github@michaelnorris.co.uk>
2014-05-29 12:23:44 +0100

    Edit footer, add directpry page

--- app/views/hello.blade.php
+++ app/views/hello.blade.php
@@ -251,75 +251,8 @@
 </div>
 
 <footer id="footer">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-10 col-md-offset-1">
-                <div class="row">
-
-                    <div class="col-md-8">
-                        <h4>
-                            Student Life A-Z Directory
-                        </h4>
-
-                        <div class="row">
-                            <div class="col-md-3">
-                                <a href="#">About the University</a><br>
-                                <a href="#">Accommodation</a><br>
-                                <a href="#">Bullying and Harassment</a><br>
-                                <a href="#">Burst Student Radio</a><br>
-                                <a href="#">Careers, Jobs and Volunteering</a><br>
-                                <a href="#">Changing Course</a><br>
-                                <a href="#">Chaplaincy</a><br>
-                                <a href="#">Childcare</a><br>
-                                <a href="#">Clubs and Societies</a><br>
-                            </div>
-                            <div class="col-md-3">
-                                <a href="#">Making a Complaint</a><br>
-                                <a href="#">Computing, Libraries and Media</a><br>
-                                <a href="#">Council Tax</a><br>
-                                <a href="#">Counselling and Wellbeing</a><br>
-                                <a href="#">Disability & Dyslexia</a><br>
-                                <a href="#">Eating, Shopping and banking</a><br>
-                                <a href="#">Enterprise and BeePurple</a><br>
-                                <a href="#">Equality and Diversity</a><br>
-                                <a href="#">Exam Stress</a><br>
-                            </div>
-                            <div class="col-md-3">
-                                <a href="#">Feedback and the NSS</a><br>
-                                <a href="#">Feeling Unwell</a><br>
-                                <a href="#">Health</a><br>
-                                <a href="#">International</a><br>
-                                <a href="#">Maps and Directions</a><br>
-                                <a href="#">Money</a><br>
-                                <a href="#">Parking and Transport</a><br>
-                                <a href="#">Safety, Fire and Security</a><br>
-                                <a href="#">Sport Brighton</a><br>
-                            </div>
-                            <div class="col-md-3">
-                                <a href="#">Student Advice Service</a><br>
-                                <a href="#">Student Charter</a><br>
-                                <a href="#">Student Regulations & Policies</a><br>
-                                <a href="#">Students' Union</a><br>
-                                <a href="#">Sustainability</a><br>
-                                <a href="#">Term Dates</a><br>
-                                <a href="#">Unicard</a><br>
-                                <a href="#">Useful Contacts </a><br>
-                            </div>
-                        </div>
-
-                    </div>
-
-                    <div class="col-md-4 text-right">
-                        <h4>WhiteBoard</h4>
-
-                        <p class="text-muted">Place sticky footer content here.</p>
-
-                        <p><a href="#">Change Year</a></p>
-                    </div>
-                </div>
-            </div>
-        </div>
-
+    <div class="container">
+        <p class="text-muted">Copyright &copy; 2014 Whiteboard.</p>
     </div>
 </footer>
 
--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -9,6 +9,7 @@
     {{ HTML::style('//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css') }}
     {{ HTML::style('//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css') }}
     {{ HTML::style('css/main.css') }}
+    {{ HTML::style('css/sticky-footer.css') }}
     <!-- Place favicon.ico and apple-touch-icon(s) in the root directory -->
 </head>
 <body>
