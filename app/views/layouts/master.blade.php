becc02706cf49c9c43eacbe713cd33cc567201e4
DuncanOgle <duncan.ogle@gmail.com>
2014-05-29 16:24:19 +0100

    Added home link to logo

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -23,7 +23,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#"><i class="fa fa-graduation-cap"></i> Whiteboard</a>
+            <a class="navbar-brand" href="/"><i class="fa fa-graduation-cap"></i> Whiteboard</a>
         </div>
         <div class="navbar-collapse collapse">
             <ul class="nav navbar-nav navbar-right">
