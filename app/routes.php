4daf98b1c530ea370afd9e9f7cc45e9732576f86
Michael Norris <github@michaelnorris.co.uk>
2014-05-29 14:43:38 +0100

    add university resources

--- app/routes.php
+++ app/routes.php
@@ -2,3 +2,9 @@
 Route::get('/', 'HomeController@showDashboard');
 Route::get('about', 'HomeController@showAbout');
 Route::resource('users', 'UsersController');
+
+Route::resource('universities', 'UniversitiesController');
+Route::resource('schools', 'SchoolsController');
+Route::resource('courses', 'CoursesController');
+Route::resource('modules', 'ModulesController');
+Route::resource('groups', 'GroupsController');
